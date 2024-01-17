245d94933664d02d19220dd34e35d87577418526
Gavin Crooks <gavin.crooks@mail.utoronto.ca>
2024-01-17 16:35:32 +0000

    Downlaoded Raw data for opendatatoronto

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,22 +1,27 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open data toronto
+# Author: Gavin Crooks
+# Date: 17 January 2024 
+# Contact: gavin.crooks@mail.utoronto.ca 
+# Pre-requisites: N/A
+
 
 
 #### Workspace setup ####
+#Load Packages
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+red_light_camera_annual_charges <- search_packages("red_light_camera_annual_charges")
+red_light_camera_annual_charges
 
+raw_red_light_camera_data <- list_package_resources("https://open.toronto.ca/dataset/red-light-camera-annual-charges/") %>% 
+  head(1) %>% 
+  get_resource()
 
+raw_red_light_camera_data
 
 #### Save data ####
 # [...UPDATE THIS...]

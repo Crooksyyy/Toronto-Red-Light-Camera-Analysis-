b173b35f962af33a5fc3130a00fae2351167494e
Gavin Crooks <gavin.crooks@mail.utoronto.ca>
2024-01-22 15:51:17 +0000

    Simulating the data set

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,27 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates Toronto red light camera data
+# Author: Gavin Crooks
+# Date: 22 January 2024
+# Contact: gavin.crooks@mail.utoronto.ca
+
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+library(data.table)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+# Always set seed 
+set.seed(93)
+
+#Creating a table with 100 different roads or 100 red light cameras
+sample_data = data.table(road = 1:100)
+# For each red light camera using any distribution pick the number of tickets issued by year
+sample_data = sample_data[, as.list(round(rnorm(13,100,10))), by = road]
+
+
+
 
 
 
